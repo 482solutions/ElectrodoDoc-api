114c55f79aac018060e6d9f294171a77aad3972f
Vladyslav Boichenko <44983140+boivlad@users.noreply.github.com>
2020-05-18 18:11:14 +0300

    Update ApiClient.js

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -48,7 +48,7 @@
      * @type {String}
      * @default http://localhost:8080/api/v1
      */
-    this.basePath = 'http://localhost:8080/api/v1'.replace(/\/+$/, '');
+    this.basePath = 'http://localhost:1823/api/v1'.replace(/\/+$/, '');
 
     /**
      * The authentication methods to be included for all API calls.
